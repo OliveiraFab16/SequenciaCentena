b7397a3902bec26f25c6a12b75ff2008994875bb
oliveiraFab16 <fabriciocarvalho932@gmail.com>
2020-11-24 21:10:48 -0300

    SequenciaCentena

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Figgle;
+
+namespace SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0 ;
+
+
+            while (numero <= 100)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Preparar...");
+                Console.ResetColor();
+                Console.Write("\n");
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Apontar...");
+                Console.ResetColor();
+                Console.Write("\n");
+            
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("FOGO!");
+                Console.ResetColor();
+                Console.WriteLine($"numero = {numero}");
+                numero ++ ;
+            }
+            Console.WriteLine(FiggleFonts.Colossal.Render("Booom"));
+            
+                  
+        }
+    }
+}
